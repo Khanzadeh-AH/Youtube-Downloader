3ea6f27a226665e65711847bb82a981578af4946
Khanzadeh_AH <khanzadeh78ah@gmail.com>
2021-05-28 23:55:01 +0430

    :bug: Seperated download views

--- app/views/streaminfo.py
+++ app/views/streaminfo.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, WebSocket, Query
-from typing import Optional
+from fastapi import APIRouter, Query
 from fastapi.templating import Jinja2Templates
 from pytube import YouTube, Playlist
 from starlette.requests import Request
@@ -9,7 +8,7 @@ templates = Jinja2Templates('app/templates')
 router = APIRouter()
 
 
-@router.get('/streams/')
+@router.get('/singlestreams/')
 async def stream(request: Request, link: str = Query(...)):
     yt = YouTube(link)
     str_streams = [str(stream_element) for stream_element in yt.streams]
@@ -22,19 +21,3 @@ async def stream(request: Request, link: str = Query(...)):
     first_video = yt.videos[0]
     str_streams = [str(stream_element) for stream_element in first_video.streams]
     return templates.TemplateResponse("streaminfo.html", {"request": request, "streams": str_streams})
-
-
-@router.get('/download/')
-async def dl(request: Request, link: str = Query(...), itag: int = Query(...)):
-    yt = YouTube(link)
-    dl_link = yt.streams.get_by_itag(itag).url
-    return templates.TemplateResponse("downloadpage.html", {"request": request, "dl_link": dl_link})
-
-
-@router.get('/playlistdownload/')
-async def dl(request: Request, link: str = Query(...), itag: int = Query(...)):
-    yt = Playlist(link)
-    download_links = []
-    for video in yt.videos:
-        download_links.append(video.streams.get_by_itag(itag).url)
-    return templates.TemplateResponse("downloadpage.html", {"request": request, "dl_links": download_links})
