c2b27e0a6b09f789684b296bd291808c4c456d75
AmirHossein Khanzadeh <khanzadeh78ah@gmail.com>
2021-05-29 14:39:57 +0430

    :bug: Bug fiexed

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 
-from app.views import home, streaminfo, downloadinfo
+from app.views import home, downloadinfo
 
 app = FastAPI()
 
 
 def configure():
     app.include_router(home.router)
-    app.include_router(streaminfo.router)
     app.include_router(downloadinfo.router)
 
 
@@ -16,4 +15,3 @@ configure()
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=5000)
-
